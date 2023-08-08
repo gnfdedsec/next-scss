60935f73050e4a7df1c2427cdabccdfee305ec00
GNF DEDSEC <ssuraw@gmail.com>
2023-08-08 17:08:58 +0700

    chage first

---
+++ components/HelloWorld.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../styles/styles.scss';
+
+function HelloWorld() {
+  return <div className="myHelloWorld">Hello, World!</div>;
+}
+
+export default HelloWorld;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HelloWorld from '../components/HelloWorld';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Next.js</h1>
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default Home;
